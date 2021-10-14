aa4b0384f9bc9c67d8cfffde3d24f0447bef43a6
rameltranquille <tranquilleramel@gmail.com>
2021-10-13 22:35:57 -0400

    normal .backup

--- backup_dots.sh
+++ backup_dots.sh
@@ -32,9 +32,9 @@ git_update () {
 }
 
 HOME="/home/ramel"
-dotfiles="$HOME/.bashrc $HOME/.zshrc $HOME/.vimrc $HOME/.config/nvim
-$HOME/.config/dmenu $HOME/.config/alacritty $HOME/.config/qtile
-$HOME/.config/st $HOME/.config/conky/conky.conf
+dotfiles="$HOME/.bashrc $HOME/.zshrc $HOME/.vimrc 
+$HOME/.config/nvim $HOME/.config/dmenu $HOME/.config/alacritty 
+$HOME/.config/qtile $HOME/.config/st $HOME/.config/conky/conky.conf
 $HOME/.config/dunst/dunstrc $HOME/.productive_xinitrc $HOME/.fun_xinitrc"
 
 scriptNprojects="$HOME/scripts/ $HOME/projects/" 
